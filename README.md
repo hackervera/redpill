2535fe71e8a3e36d5f80d2e3d0c15c320b1484f6
Tyler Gillies <tjgillies@gmail.com>
2015-03-14 21:10:27 -0700

    configurable mappings

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ You need to update your homeserver data to include application password. From yo
 You also need to run `ruby intialize_app.rb` once to initialize app and then run `ruby webserver.rb` to start server.
 
 
-Once everything is running you should be able to join ``#irc/your-channel:example.com` inside Matrix and the bot will automatically join `your-channel:example.com` on the configured irc server when you send your first message.
+Once everything is running you should be able to join ``#irc/your-channel:example.com` inside Matrix and the bot will automatically join the corresponding channel on irc that is configured in your settings
--- webserver.rb
+++ webserver.rb
@@ -11,11 +11,12 @@ class Webserver < Sinatra::Base
   matrix = Matrix.new(config["host"])
   token = matrix.login(config["user"], config["password"])
   callback = lambda do |m|
-    channel = m.channel.name.gsub("#","#irc/")
+    channel_name = m.channel.name
+    matrix_channel = config["channel_map"].detect{|c| c["irc_channel"] == channel_name}["matrix_channel"]
     nick = m.user.nick
-    host = channel.split(":").last
+    host = matrix_channel.split(":").last
     user = "@irc/#{nick}:#{host}"
-    room_id = matrix.room_id(channel, token)
+    room_id = matrix.room_id(matrix_channel, token)
     matrix.join(room_id, config["app_password"], user)
     matrix.send_message(room_id, m.message, config["app_password"], user)
   end
@@ -27,18 +28,21 @@ class Webserver < Sinatra::Base
   end
     
   put "/transactions/:transaction" do
+    config = YAML.load_file('config.yaml')
     events = JSON.parse(request.body.read)["events"]
     puts events
     data = events.first
+    return {}.to_json unless data["type"] == "m.room.message"
     message = data["content"]["body"]
     nick = data["user_id"]
     host = data["room_id"].split(":").last
-    room_alias = matrix.alias(data["room_id"], token).split("/").last
-    channel = irc.bot.Channel("#"+room_alias)
+    room_alias = matrix.alias(data["room_id"], token)
+    irc_channel = config["channel_map"].detect{|c| c["matrix_channel"] == room_alias}["irc_channel"]
+    channel = irc.bot.Channel(irc_channel)
     if in_channel?(irc, channel.name)
       channel.send("#{nick}: #{message}") unless nick =~ /irc/
     else
-      irc.bot.join("#"+room_alias)
+      irc.bot.join(irc_channel)
       channel.send("#{nick}: #{message}") unless nick =~ /irc/
     end
     {}.to_json
